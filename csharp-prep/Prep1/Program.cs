30dbb22483bd8c243cafeb27e1afd8c9adbadc82
Anderson-Damen <Andersondamen@gmail.com>
2024-01-16 12:51:04 -0700

    First Commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask user for first name
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        //Ask user for last name
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        
+        //Print out the phrase.
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your currect grade percentage?");
+        string userinput = Console.ReadLine();
+        int grade = int.Parse(userinput);
+        
+        string letter = "q";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Grade is a {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You Passed!");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("Sorry you didn't pass, but try again, you got this!");
+        }
     }
 }
