f514a651e868d0c320faf3d51c27f30e6091e23e
Anderson-Damen <Andersondamen@gmail.com>
2024-01-18 14:48:31 -0700

    Prep3 Commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        
+        string loop = "yes";
+        
+        while (loop == "yes")
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            int numberGuess = int.Parse(userGuess);
+        
+            if (numberGuess > magicNumber)
+            {
+              Console.WriteLine("Lower");
+              loop = "yes";
+            }
+            else if (numberGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                loop = "yes";
+            }
+            else if (numberGuess == magicNumber)
+            {
+                Console.Write("You guessed it!");
+                loop = "no";
+            }
+        }
     }
 }
