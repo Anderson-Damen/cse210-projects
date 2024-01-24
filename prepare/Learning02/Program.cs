f0ae998c6aa755bc92db64dd3588fe3f051a9c2b
Anderson-Damen <Andersondamen@gmail.com>
2024-01-24 14:41:03 -0700

    Second Commit, the main program is not recognizing the other classes that have been made.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,6 +9,19 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2024;
         job1._endYear = 2028;
-        job1.Display();
+
+        job1 job2 = new Job();
+        job2._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Damen Anderson";
+
+        myResume._jobs.Add(jobs1);
+        myResume._jobs.Add(jobs2);
+
+        myResume.Display();
     }
 }
