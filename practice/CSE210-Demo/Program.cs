a6ac5582f6e16d4b0f9ce67e1c3b1056da2d4007
Anderson-Damen <Andersondamen@gmail.com>
2024-01-24 14:25:03 -0700

    First push for Learing2

---
+++ practice/CSE210-Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿Person person1 = new Person();
+
+person1._givenName = "Damen";
+person1._familyName = "Anderson";
+
+//person1.ShowEasternName();
+//person1.ShowWesternName();
+
+person2._givenName = "Makenna";
+person2._familyName = "Anderson";
+
+//people.Add(person1);
+//people.Add(person2);
+
+foreach(Person person in people)
+{
+    person.ShowWesternName
+}
