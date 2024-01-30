3489876ad35625bfea7aa35c903e8ae054223bf7
Anderson-Damen <Andersondamen@gmail.com>
2024-01-30 14:43:19 -0700

    In class coding for Develope 2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string userChoice = "1";
+
+        while (userChoice != "5")
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices");
+            Console.WriteLine(" 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5. Quit");
+
+            userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "1":
+                    Console.WriteLine("Write");
+                    break;
+                case "2":
+                    Console.WriteLine("Display");
+                    break;
+                case "3":
+                    Console.WriteLine("load");
+                    break;
+                case "4":
+                    Console.WriteLine("Save");
+                    break;
+                case "5":
+                    Console.WriteLine("Display");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a number between 1 and 5");
+                    break;
+            }
+        }
     }
 }
